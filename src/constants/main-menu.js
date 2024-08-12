f71838345431fcd4d6e260bb5ca45669e58c1686
arty gvozdenkov <gvozdenkov@gmail.com>
2024-08-12 12:15:22 +0300

    refactor: main menu config array

---
+++ src/constants/main-menu.js
@@ -0,0 +1,18 @@
+export const mainMenu = [
+  {
+    link: 'profile',
+    text: 'Личный кабинет',
+  },
+  {
+    link: 'blog',
+    text: 'Блог',
+  },
+  {
+    link: 'policy',
+    text: 'Политика конфиденциальност',
+  },
+  {
+    link: 'contacts',
+    text: 'Контакты',
+  },
+];
--- src/widgets/header.jsx
+++ src/widgets/header.jsx
@@ -1,15 +1,15 @@
+import { mainMenu } from '#constants/main-menu.js';
 import { NavLink } from 'react-router-dom';
 
 export const Header = () => {
   return (
     <>
-      <ul style={{ borderBottom: '1px solid gray', margin: 0 }}>
-        <li>
-          <NavLink to="profile">Profile</NavLink>
-        </li>
-        <li>
-          <NavLink to="blog">Blog</NavLink>
-        </li>
+      <ul style={{ borderBottom: '1px solid gray', margin: 0, display: 'flex', gap: '20px' }}>
+        {mainMenu.map((menu) => (
+          <li key={menu.link}>
+            <NavLink to={menu.link}>{menu.text}</NavLink>
+          </li>
+        ))}
       </ul>
     </>
   );
